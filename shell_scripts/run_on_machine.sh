d85e840ef373a63945ff02b06ba8d5bed71a5eb2
Hankin-Liu <wojiaoliuhuajun@126.com>
2024-07-22 16:48:58 +0800

    print info before and after run, comments the print

--- shell_scripts/run_on_machine.sh
+++ shell_scripts/run_on_machine.sh
@@ -7,7 +7,7 @@ fi
 
 which sshpass > /dev/null
 if [[ $? -ne 0 ]];then
-    echo "run_on_machine depends on sshpass, please install it."
+    echo "run_on_machine depends on sshpass and which, please install it."
     exit -1
 fi
 
@@ -16,5 +16,6 @@ user=$2
 password=$3
 command=$4
 
+#echo "[START] sshpass -p $password ssh -o StrictHostKeyChecking=no ${user}@${ip} \"sh -c \"$command\"\""
 sshpass -p $password ssh -o StrictHostKeyChecking=no ${user}@${ip} "sh -c \"$command\""
-#echo "cmd:::sshpass -p $password ssh -o StrictHostKeyChecking=no ${user}@${ip} sh -c $command"
+#echo "[FINISHED] sshpass -p $password ssh -o StrictHostKeyChecking=no ${user}@${ip} \"sh -c \"$command\"\""
